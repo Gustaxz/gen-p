20e08e61c5e9a38dfd30733cf59bd8d0a2863fc0
Gustaxz <guxtavogusteible@gmail.com>
2023-12-18 12:10:58 -0300

    feat: adding pino logger

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,27 +1,34 @@
-function info(message: string) {
-	console.log(message)
-}
+// function info(message: string) {
+// 	console.log(message)
+// }
 
-function error(message: string) {
-	console.error(message)
-}
+// function error(message: string) {
+// 	console.error(message)
+// }
 
-function warn(message: string) {
-	console.warn(message)
-}
+// function warn(message: string) {
+// 	console.warn(message)
+// }
 
-function debug(message: string) {
-	console.debug(message)
-}
+// function debug(message: string) {
+// 	console.debug(message)
+// }
 
-function log(message: string) {
-	console.log(message)
-}
+// function log(message: string) {
+// 	console.log(message)
+// }
 
-export const logger = {
-	info,
-	error,
-	warn,
-	debug,
-	log,
-}
+// export const logger = {
+// 	info,
+// 	error,
+// 	warn,
+// 	debug,
+// 	log,
+// }
+
+import pino from "pino"
+import pretty from "pino-pretty"
+
+const logger = pino(pretty())
+
+export { logger }
