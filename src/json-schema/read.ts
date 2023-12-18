122ce3021c5b3eb637f15c6d2614c3697deb3bac
Gustaxz <guxtavogusteible@gmail.com>
2023-12-18 11:56:53 -0300

    feat: adding better error handling for read json

--- src/json-schema/read.ts
+++ src/json-schema/read.ts
@@ -1,39 +1,50 @@
 import { readFile } from "fs/promises"
 import { translatePrismaToTypescript } from "../utils/translate-prisma-ts"
 import { PrismaModel } from "./types"
+import { logger } from "../logger"
 
 async function readJsonFile(path: string) {
-	const jsonFile = await readFile(path, "utf-8")
-	return JSON.parse(jsonFile)
+	try {
+		const jsonFile = await readFile(path, "utf-8")
+		return JSON.parse(jsonFile)
+	} catch (err: any) {
+		logger.error("error running readJsonFile " + err.message)
+		throw new Error(err)
+	}
 }
 
 export async function parseJsonSchema(path: string) {
-	const jsonSchema = await readJsonFile(path)
+	try {
+		const jsonSchema = await readJsonFile(path)
 
-	const definitions = jsonSchema.definitions
+		const definitions = jsonSchema.definitions
 
-	const modelName = Object.keys(definitions).map((key) => key)[0]
-	const modelProperties = definitions[modelName].properties
-	const modelPropertiesKeys: PrismaModel[] = []
+		const modelName = Object.keys(definitions).map((key) => key)[0]
+		const modelProperties = definitions[modelName].properties
+		const modelPropertiesKeys: PrismaModel[] = []
 
-	Object.keys(modelProperties).forEach((key) => {
-		const object: PrismaModel = { name: "", type: "" }
+		Object.keys(modelProperties).forEach((key) => {
+			const object: PrismaModel = { name: "", type: "" }
 
-		object.name = key
+			object.name = key
 
-		if (modelProperties[key].type instanceof Array) {
-			object.type = modelProperties[key].type.map((type: string) =>
-				translatePrismaToTypescript(type)
-			)
-		} else {
-			object.type = translatePrismaToTypescript(modelProperties[key].type)
+			if (modelProperties[key].type instanceof Array) {
+				object.type = modelProperties[key].type.map((type: string) =>
+					translatePrismaToTypescript(type)
+				)
+			} else {
+				object.type = translatePrismaToTypescript(modelProperties[key].type)
+			}
+
+			modelPropertiesKeys.push(object)
+		})
+
+		return {
+			name: modelName,
+			properties: modelPropertiesKeys,
 		}
-
-		modelPropertiesKeys.push(object)
-	})
-
-	return {
-		name: modelName,
-		properties: modelPropertiesKeys,
+	} catch (err: any) {
+		logger.error("error running parseJsonSchema " + err.message)
+		throw new Error(err)
 	}
 }
